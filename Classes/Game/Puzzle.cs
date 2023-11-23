fa8556223d7bfc9f129133c390c205bfc66131af
MrNinjaPowha <you@example.com>
2023-11-23 11:30:18 +0100

    Added movement in cardinal directions

--- Classes/Game/Puzzle.cs
+++ Classes/Game/Puzzle.cs
@@ -47,17 +47,77 @@ internal class Puzzle
                     SelectedStick.Empty = !SelectedStick.Empty;
                     break;
                 case ConsoleKey.LeftArrow:
-                    stickX--;
-                    break;
-                case ConsoleKey.UpArrow:
-                    stickY--;
+                    MoveLeft();
                     break;
                 case ConsoleKey.RightArrow:
-                    stickX++;
+                    MoveRight();
+                    break;
+                case ConsoleKey.UpArrow:
+                    MoveUp();
                     break;
                 case ConsoleKey.DownArrow:
-                    stickY++;
+                    MoveDown();
                     break;
+            }
+        }
+    }
+
+    private void MoveLeft()
+    {
+        List<IStick> row = equation.Sticks[stickY];
+
+        for (int i = stickX - 1; i >= 0; i--)
+        {
+            if (row[i].Empty == holdingStick)
+            {
+                stickX = i;
+                break;
+            }
+        }
+    }
+
+    private void MoveRight()
+    {
+        List<IStick> row = equation.Sticks[stickY];
+
+        for (int i = stickX + 1; i < row.Count; i++)
+        {
+            if (row[i].Empty == holdingStick)
+            {
+                stickX = i;
+                break;
+            }
+        }
+    }
+
+    private void MoveUp()
+    {
+        for (int i = stickY - 1; i >= 0; i--)
+        {
+            double widthScale = (double)equation.Sticks[i].Count / equation.Sticks[stickY].Count;
+            int column = (int)(stickX * widthScale);
+
+            if (equation.Sticks[i][column].Empty == holdingStick)
+            {
+                stickY = i;
+                stickX = column;
+                break;
+            }
+        }
+    }
+
+    private void MoveDown()
+    {
+        for (int i = stickY + 1; i < equation.Sticks.Count; i++)
+        {
+            double widthScale = (double)equation.Sticks[i].Count / equation.Sticks[stickY].Count;
+            int column = (int)(stickX * widthScale);
+
+            if (equation.Sticks[i][column].Empty == holdingStick)
+            {
+                stickY = i;
+                stickX = column;
+                break;
             }
         }
     }
